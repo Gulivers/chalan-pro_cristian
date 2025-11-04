386ad04abb3d4273ddbfcc9b863c91e47d2cacb3
Oliver <oliveralverto@gmail.com>
2025-11-04 00:15:03 -0500

    Fix: Correct ASGI settings module and improve Render build configuration

--- project/asgi.py
+++ project/asgi.py
@@ -4,7 +4,7 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 import appschedule.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chalan_pro.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
