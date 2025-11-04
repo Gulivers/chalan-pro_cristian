c1bc4669ff6387d329309e7d542d356e3f5bdc86
Oliver <oliveralverto@gmail.com>
2025-11-03 23:49:30 -0500

    Fix: Agregar STATIC_ROOT para collectstatic en Render

--- project/settings.py
+++ project/settings.py
@@ -260,6 +260,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# STATIC_ROOT es donde Django recopilará todos los archivos estáticos para producción
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
